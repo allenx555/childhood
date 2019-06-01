9f6594a10c27c73b5145ae18bab34dcfd1f956d4
allenx555 <allenx555@163.com>
2019-06-01 16:48:10 +0800

    feat: add homepage

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# JoTang News Knowledge Graph
+# ChildHood
 
 主要技术栈：
 
-    前端：React + Echarts
+    前端：React.mobile
     后端：Flask + MySQL
 
 ## 在线演示
@@ -13,7 +13,7 @@
 
 #### 前端运行
 
-    git clone https://github.com/allenx555/JoTang-News-KnowledgeGraph.git
+    git clone https://github.com/allenx555/childhood.git
     yarn # or npm i
     yarn dev # or npm run dev
 
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../assets/home.scss";
+import { Button } from "antd-mobile";
 
 
 export default class Home extends React.Component {
   render() {
     return (
       <div className="container">
+        <Button type="primary" style={{float:"left","margin-right":"30px","width":"120px"}}>a</Button>
+        <Button type="primary" style={{float:"right","width":"120px"}}>b</Button>
       </div>
     );
   }
