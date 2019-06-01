9a05e8e8cedaf81e55347eb05211cc07f1171072
allenx555 <allenx555@163.com>
2019-06-02 02:49:24 +0800

    feat: add diary css and data

--- src/components/diarylist.jsx
+++ src/components/diarylist.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import circle from "../static/circle.png";
+import "../assets/diarylist.scss";
 
-export default class diarylist extends React.Component {
-  state = {
-    year: "",
-    diarys: [{ diarydate: "", diarytitle: "", diaryurl: "" }]
-  };
+export default class Diarylist extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>
-        <div className="circle" />
-        <div className="diarycontent" />
+        <div className="circle">
+          <img src={circle} alt="circle" />
+        </div>
+        <div className="diarycontent">
+          <h1>{this.props.arr.year}</h1>
+          {this.props.arr.diarys.map(data => (
+            <a href={data.diaryurl}>
+              <pre>
+                {data.diarydate}&nbsp;&nbsp;&nbsp;
+                {data.diarytitle}
+              </pre>
+            </a>
+          ))}
+        </div>
       </div>
     );
   }
--- src/views/Diary.jsx
+++ src/views/Diary.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import more from "../static/more.png";
 import plus from "../static/plus.png";
 import "../assets/diary.scss";
+import Diarylist from "../components/diarylist";
+import json from "../static/test.json";
 
 export default class Diary extends React.Component {
+  state = {
+    arr: json
+  };
   render() {
     return (
       <div id="content">
@@ -14,7 +19,16 @@ export default class Diary extends React.Component {
             style={{ "margin-top": 55, width: 96, float: "left" }}
           />
         </div>
-        <div id="right" />
+        <div id="right">
+          {this.state.arr.map(data => (
+            <Diarylist arr={data} />
+          ))}
+        </div>
+        <div><img
+            src={plus}
+            alt="plus"
+            style={{  "margin-top": 580,float: "right" }}
+          /></div>
       </div>
     );
   }
